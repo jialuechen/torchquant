ab6ea60b1908054ce44d03e0a3395342ee4326a4
Jialue Chen <jialuechen@outlook.com>
2022-03-09 00:01:42 -0500

    add comments for greeks

--- quantorch/greeks.py
+++ quantorch/greeks.py
@@ -1,36 +1,39 @@
 import torch
 
-# For the kwargs below, we required the all the values in the dictionary are tensors
+'''
+For the kwargs below, we required the all the values in the dictionary are tensors.
+Usually the basic keys in the kwargs are spot,volatility,maturity and rate. The keys should meet the requirements of the pricer.
+'''
 
 @torch.enable_grad
-def delta(pricing_model,create_graph:bool=False,**kwargs)->torch.tensor:
+def delta(pricer,create_graph:bool=False,**kwargs)->torch.tensor:
     # Please note that here the spot is a tensor
     spot=kwargs["spot"].requires_grad_()
-    price=pricing_model(**kwargs)
+    price=pricer(**kwargs)
     return torch.autograd.grad(price,inputs=spot,grad_outputs=torch.ones_like(price),create_graph=create_graph)[0]
 
 @torch.enable_grad
-def gamma(pricing_model,create_graph:bool =False,**kwargs)->torch.tensor:
+def gamma(pricer,create_graph:bool =False,**kwargs)->torch.tensor:
     spot=kwargs["spot"]
     # set create_graph as True to allow the computation of the second order derivatives
-    the_delta=delta(pricing_model,create_graph=True,**kwargs).requires_grad_()
+    the_delta=delta(pricer,create_graph=True,**kwargs).requires_grad_()
     return torch.autograd.grad(the_delta,inputs=spot,grad_outputs=torch.ones_like(the_delta),create_graph=create_graph)[0]
 
 @torch.enable_grad
-def vega(pricing_model,create_graph:bool=False,**kwargs)->torch.tensor:
-    price=pricing_model(**kwargs)
+def vega(pricer,create_graph:bool=False,**kwargs)->torch.tensor:
+    price=pricer(**kwargs)
     vol=kwargs["vol"].requires_grad_()
     return torch.autograd.grad(price,inputs=vol,grad_outputs=torch.ones_like(price),create_graph=create_graph)[0]
     
 
 @torch.enable_grad
-def theta(pricing_model,create_graph:bool=False,**kwargs)->torch.tensor:
+def theta(pricer,create_graph:bool=False,**kwargs)->torch.tensor:
     maturity=kwargs["maturity"].requires_grad_()
-    price=pricing_model(**kwargs)
+    price=pricer(**kwargs)
     return -torch.autograd.grad(price,inputs=maturity,grad_outputs=torch.ones_like(price), create_graph=create_graph)[0]
 
 @torch.enable_grad
-def rho(pricing_model,create_graph:bool=False,**kwargs)->torch.tensor:
+def rho(pricer,create_graph:bool=False,**kwargs)->torch.tensor:
     rate=kwargs["rate"].requires_grad_()
-    price=pricing_model(**kwargs)
+    price=pricer(**kwargs)
     return -torch.autograd.grad(price,inputs=rate,grad_outputs=torch.ones_like(price), create_graph=create_graph)[0]
