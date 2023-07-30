919ae5f18ee245ceed3fa5685668fd49def4aa12
Jialue Chen <jialuechen@outlook.com>
2023-07-30 15:36:39 -0400

    update Binomal Tree Model

--- quantorch/models/discrete/binomial.py
+++ quantorch/models/discrete/binomial.py
@@ -1,5 +1,15 @@
-import torch
+from torch import Tensor,exp
 
-class Binomal():
-    def __init__(self) -> None:
+class BinomalTree():
+    def __init__(self,period:Tensor,spot:Tensor,u:Tensor,d:Tensor,rate:Tensor,) -> None:
+        self.period=period
+        self.spot=spot
+        self.u=u
+        self.d=d
+        self.rate=rate
+
+    def getRiskNeutralProb(self)->Tensor:
+        return [(exp(self.rate)-self.d)/(self.u-self.d),1-(exp(self.rate)-self.d)/(self.u-self.d)]
+    
+    def getRiskNeutralDelta(self)->Tensor:
         pass
