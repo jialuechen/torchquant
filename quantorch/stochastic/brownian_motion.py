081ddcfff84c77043bd45d6f4c34611ce11617e0
Jialue Chen <jialuechen@outlook.com>
2023-07-30 13:51:16 -0400

    update Brownian Motion

--- quantorch/stochastic/brownian_motion.py
+++ quantorch/stochastic/brownian_motion.py
@@ -5,7 +5,7 @@ from torch import Tensor
 '''
 generate brownian motion
 '''
-def brownian_motion(size,time,init_value:float=0.0,drift:float=0.0,volatility:float=0.0,device=None)->Tensor:
+def brownian_motion(size,time:Tensor,init_value:Tensor,drift:Tensor,volatility:Tensor,device=None)->Tensor:
     dt=torch.empty(time)
     dwt=dt.sqrt()
     return init_value+drift*time+(volatility*torch.randn(size)*dwt).cumsum(dim=-1)
@@ -13,5 +13,5 @@ def brownian_motion(size,time,init_value:float=0.0,drift:float=0.0,volatility:fl
 '''
 generate geometric brownian motion
 '''
-def geometric_brownian_motion(size,time,init_value:float=0.0,drift:float=0.0,volatility:float=0.0,device=None)->Tensor:
+def geometric_brownian_motion(size,time:Tensor,init_value:Tensor,drift:Tensor,volatility:Tensor,device=None)->Tensor:
     return init_value*torch.exp(brownian_motion(size,time,drift-volatility**2/2,volatility,device=device))
