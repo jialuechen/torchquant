f922947c08ca079be3c20a6eac51201e5df3c4ba
Jialue Chen <jialuechen@outlook.com>
2024-07-20 20:29:39 -0400

    fixed license

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/yourusername/quantorch',
     classifiers=[
         'Programming Language :: Python :: 3.8',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache-2.0 License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.8',
