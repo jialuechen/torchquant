43256a1ff3fab714673fb50a9d362440e7fec68d
Jialue Chen <jialuechen@outlook.com>
2024-09-30 12:21:45 -0400

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '1.2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc','sphinx.ext.viewcode']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
