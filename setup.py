da960e1d410fcb1033774ec7508d385ffc638ba1
Jialue Chen <jialuechen@outlook.com>
2025-08-08 20:49:51 -0400

    udates

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# ...existing code...
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -33,4 +32,3 @@ setup(
     ],
     python_requires='>=3.7',
 )
-# ...existing code...
